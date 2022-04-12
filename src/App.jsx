e727eecdf90cfca7d8375a08ab47dee42ff96a31
Justin Vongdara <vongdara16@gmail.com>
2022-04-12 15:22:02 -0400

    sleep cards work

--- src/App.jsx
+++ src/App.jsx
@@ -36,18 +36,18 @@ const App = () => {
     setUser(authService.getUser())
   }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await sleepService.getAll()
-      console.log(data)
-      data.forEach(sleep => {
-        if (user.id === sleep.profile_id) {
-          setSleepLogs([sleep, ...sleepLogs])
-        }
-      })
-    }
-    fetchData()
-  }, [])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await sleepService.getAll()
+  //     console.log('all', data)
+  //     console.log('1', data[0].profile_id)
+  //     console.log('user', user)
+  //     console.log('user.id', user.id)
+      
+
+  //   }
+  //   fetchData()
+  // }, [])
 
   return (
     <>
@@ -76,6 +76,7 @@ const App = () => {
               <SleepList
                 user={user}
                 sleepLogs={sleepLogs}
+                setSleepLogs={setSleepLogs}
               />
             </ProtectedRoute>
           }
