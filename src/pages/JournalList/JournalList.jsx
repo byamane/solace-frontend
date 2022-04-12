951ce9d5f43aec76d85aaca360c417732226d58d
byamane <byamane21@gmail.com>
2022-04-12 15:47:15 -0500

    update journal index functionality for privacy

--- src/pages/JournalList/JournalList.jsx
+++ src/pages/JournalList/JournalList.jsx
@@ -1,8 +1,21 @@
 import NavBarBot from "../../components/NavBarBot/NavBarBot";
 import JournalCard from "../../components/JournalCard/JournalCard";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import * as journalService from '../../services/journalService'
 
 const JournalList = (props) => {
+  console.log('props', props)
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await journalService.getAll()
+      console.log('data', data)
+      props.setJournalEntries(data)
+    }
+    fetchData()
+  }, [])
+  
   return (  
     <>
       <h1>Your Journal</h1>
