d75e19e075eb936fd9c6a13ae22299df9cf2a2fb
byamane <byamane21@gmail.com>
2022-04-21 17:07:04 -0500

    style journal details for bigger screens

--- src/pages/JournalDetails/JournalDetails.jsx
+++ src/pages/JournalDetails/JournalDetails.jsx
@@ -20,21 +20,23 @@ const JournalDetails = (props) => {
       <div id="journal-details-btns">
         <JournalActions journal={journal} user={props.user} />
       </div>
-      <div id="journal-details-container">
-        <div id="journal-details-title">
-          <h3>{journal.name}</h3>
-        </div>
-        <div>
-          <h3>
-            Date: {journalDate}
-          </h3>
-        </div>
-        <div id="journal-details-rating">
-          <h3>Mood: {emojiIndex[journal.mood]}</h3>
-        </div>
-        <div id="journal-details-journal">
-          <h3>Message: </h3>
-          <p>{journal.journal}</p>
+      <div id="journal-details-div">
+        <div id="journal-details-container">
+          <div id="journal-details-title">
+            <h3>{journal.name}</h3>
+          </div>
+          <div>
+            <h3>
+              Date: {journalDate}
+            </h3>
+          </div>
+          <div id="journal-details-rating">
+            <h3>Mood: {emojiIndex[journal.mood]}</h3>
+          </div>
+          <div id="journal-details-journal">
+            <h3>Message: </h3>
+            <p>{journal.journal}</p>
+          </div>
         </div>
       </div>
     </>
